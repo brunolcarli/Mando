882c4984090622773bfeded6c3fbceb838a61639
Bruno L. Carli <brunolcarli@gmail.com>
2020-02-09 14:57:50 -0300

    CHANGE revision version

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ Módulo para configurações do Mando
 """
 from decouple import config
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 TOKEN = config('TOKEN', '')
 API_URL = config('STAR_DESTROYER', '')
