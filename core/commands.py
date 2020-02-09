559fe087cc931ceadf71c70c3f2836d0e6122f19
Bruno L. Carli <brunolcarli@gmail.com>
2020-02-09 14:08:50 -0300

    ADD basic commands

---
+++ core/commands.py
@@ -0,0 +1,58 @@
+r"""
+Módulo dedicado aos comandos Discord que o Mando disponibiliza
+"""
+from random import randint
+import discord
+from discord.ext import commands
+
+
+client = commands.Bot(command_prefix='--')
+
+
+@client.event
+async def on_ready():
+    print('Ready!')
+
+
+@client.command()
+async def ping(discord):
+    """
+    Pinga o bot para teste sua execução
+    """
+    await discord.send('pong!')
+
+
+@client.command()
+async def roll(discord, sides=None):
+    """
+    Rola um dado e retornando o resultado.
+    Por padrão o dado posusi 6 lados, fornecendo um valor Mando rolará um
+    dado esta quantidade de lados.
+    Exemplo
+        - `--roll 20`
+    """
+    # Se nenhum argumento for fornecido rola um dado de 6 lados
+    if not sides:
+        result = randint(1, 6)
+        return await discord.send(
+            f'Em um dado de 6 lados {discord.author.name} rolou {result}!'
+        )
+
+    # Mensagem de erro padrão em caso de entrada de valor inválido
+    invalid_value_error = 'Esta valor é inválido, por favor forneça um número!'
+
+    # tenta realizar a conversão do argumento fornecido para um inteiro absoluto
+    try:
+        sides = abs(int(sides))
+    except ValueError:
+        # Se a conversão falhar o usuário forneceu um valor inválido
+        return await discord.send(invalid_value_error)
+
+    # Rola o dado com o valor fornecido, desde que não seja zero
+    if not sides:
+        return await discord.send(invalid_value_error)
+
+    result = randint(1, sides)
+    return await discord.send(
+        f'Em um dado de {sides} lados {discord.author.name} rolou {result}!'
+    )
